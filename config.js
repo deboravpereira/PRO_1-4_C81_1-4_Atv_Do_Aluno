2d74c4e7937ef5023c70336eb64d25caf39d73ae
deboravpereira <100171504+deboravpereira@users.noreply.github.com>
2023-11-22 12:47:39 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,8 @@
 import firebase from "firebase";
 require("@firebase/firestore");
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDKsBvE1fSCPfG8CwxAnVkuyY2zkTXcNUw",
-  authDomain: "bibliotecaeletronica-5f78b.firebaseapp.com",
-  projectId: "bibliotecaeletronica-5f78b",
-  storageBucket: "bibliotecaeletronica-5f78b.appspot.com",
-  messagingSenderId: "194185416510",
-  appId: "1:194185416510:web:5ac2215b8b8f048a23266f"
-};
+//Coloque aqui seu SDK
+
 
 firebase.initializeApp(firebaseConfig);
 
